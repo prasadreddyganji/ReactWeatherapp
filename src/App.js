2a418cc98015faf86d8f249a8d4776c3da7ad713
prasadreddyganji <prasadreddyganji461@gmail.com>
2023-05-17 21:43:14 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const API_KEY = 'YOURAPIKEY'; // Replace with your own API key
+
+const App = () => {
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState('');
+
+  const fetchWeatherData = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      );
+      setWeatherData(response.data);
+      setCity('');
+    } catch (error) {
+      console.error(error);
+      setWeatherData(null);
+      setCity('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Fuck OFF LEELA PRASANNA</h1>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Enter city name"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button onClick={fetchWeatherData}>Get Weather</button>
+      </div>
+      {weatherData && weatherData.main && (
+        <div className="weather-container">
+          <h2>{weatherData.name}</h2>
+          <div className="weather-info">
+            <p className="temperature">{Math.round(weatherData.main.temp)}°C</p>
+            <div className="weather-details">
+              <p>Humidity: {weatherData.main.humidity}%</p>
+              {weatherData.weather.length > 0 && (
+                <p>Weather: {weatherData.weather[0].main}</p>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
+
